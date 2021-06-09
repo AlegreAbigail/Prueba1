356114ce38f8ecd1c003547da00454ed00052ebf
AlegreAbigail <abigailalegre18@gmail.com>
2021-06-09 16:33:03 -0300

    Agrego todo el proyecto

---
+++ Grupo-19-OO2-2021/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO perfil values(1,null,"ROLE_ADMIN",null); 
+INSERT INTO perfil values(2,null,"ROLE_USER",null);
+
+INSERT INTO persona values(1,"Alegre",4200000,"Abigail","abi");
+INSERT INTO persona values(2,"Alegre",4200000,"Angeles","angeles");
+INSERT INTO persona values(3,"Alegre",4200000,"Angeles","auditor");
+
+INSERT INTO usuario values(1,1,"Alegre",null,"profe@gmail.com","Abigail","abi",4200000,"$2a$10$qc3bq8fWVpixDSCaB7.zN.7HsgDio76EA9m4nuPF5scWbbPtHrE/m",1,1,null,1,1);
+INSERT INTO usuario values(2,1,"Alegre",null,"profe@gmail.com","Angeles","auditor",4200000,"$2a$10$qc3bq8fWVpixDSCaB7.zN.7HsgDio76EA9m4nuPF5scWbbPtHrE/m",1,2,null,2,3);
+
+INSERT INTO lugar values(1,1854,"CABA"); 
+INSERT INTO lugar values(2,1765,"Mar del plata"); 
+INSERT INTO lugar values(3,1856,"Bariloche"); 
+INSERT INTO lugar values(4,1854,"Lanús"); 
+INSERT INTO lugar values(5,1765,"Temperley"); 
+INSERT INTO lugar values(6,1856,"Constitución");
